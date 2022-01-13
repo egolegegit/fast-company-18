ba3e20480dc529ea671a237f8945c8396dd06b64
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-01-13 14:19:20 +0700

    7. Создаем Users Slice

---
+++ src/app/store/users.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import userService from "../services/user.service";
+
+const usersSlice = createSlice({
+    name: "users",
+    initialState: {
+        entities: null,
+        isloading: true,
+        error: null
+    },
+    reducers: {
+        usersReceved: (state, action) => {
+            state.entities = action.payload;
+            state.lastFetch = Date.now();
+            state.isloading = false;
+        },
+        usersRequested: (state) => {
+            state.isLoading = true;
+        },
+        usersRequestedFailed(state, action) {
+            state.error = action.payload;
+            state.isLoading = false;
+        }
+    }
+});
+
+const { reducer: usersReducer, actions } = usersSlice;
+const { usersRequested, usersRequestedFailed, usersReceved } = actions;
+
+export const loadUsersList = () => async (dispatch, getState) => {
+    const { lastFetch } = getState().users;
+    console.log(lastFetch);
+
+    dispatch(usersRequested());
+
+    try {
+        const { content } = await userService.get();
+        dispatch(usersReceved(content));
+    } catch (error) {
+        dispatch(usersRequestedFailed(error.message));
+    }
+};
+
+export default usersReducer;
