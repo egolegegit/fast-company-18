574f661c49ca0c301aebf9dc443f1adec8d40837
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-01-13 21:42:50 +0700

    9. Переносим метод регистрации в User Slice

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import authService from "../services/auth.service";
+import localStorageService from "../services/localStorage.service";
 import userService from "../services/user.service";
 
 const usersSlice = createSlice({
@@ -6,7 +8,9 @@ const usersSlice = createSlice({
     initialState: {
         entities: null,
         isloading: true,
-        error: null
+        error: null,
+        auth: null,
+        isLoggedIn: false
     },
     reducers: {
         usersReceved: (state, action) => {
@@ -20,12 +24,24 @@ const usersSlice = createSlice({
         usersRequestedFailed(state, action) {
             state.error = action.payload;
             state.isLoading = false;
+        },
+        authRequestSuccess: (state, action) => {
+            state.auth = { ...action.payload, isLoggedIn: true };
+        },
+        authRequestFailed: (state, action) => {
+            state.error = action.payload;
         }
     }
 });
 
 const { reducer: usersReducer, actions } = usersSlice;
-const { usersRequested, usersRequestedFailed, usersReceved } = actions;
+const {
+    usersRequested,
+    usersRequestedFailed,
+    usersReceved,
+    authRequestSuccess,
+    authRequestFailed
+} = actions;
 
 export const loadUsersList = () => async (dispatch, getState) => {
     const { lastFetch } = getState().users;
@@ -46,6 +62,21 @@ export const getUserById = (userId) => (state) => {
         return state.users.entities.find((item) => item._id === userId);
     }
 };
+
+const authRequested = createAction("users/requested");
+
+export const signUp =
+    ({ email, password, ...rest }) =>
+    async (dispatch) => {
+        dispatch(authRequested());
+        try {
+            const data = await authService.register({ email, password });
+            localStorageService.setTokens(data);
+            dispatch(authRequestSuccess({ userId: data.localId }));
+        } catch (error) {
+            dispatch(authRequestFailed(error.message));
+        }
+    };
 
 export const getUsersList = () => (state) => state.users.entities;
 export default usersReducer;
